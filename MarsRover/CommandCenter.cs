9feee7ca6f1350dcc1d486d201e592ac584b47bb
richardhallett <rickhallett@live.co.uk>
2020-02-11 08:52:59 +0000

    first commit

---
+++ MarsRover/CommandCenter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace MarsRover
+{
+    public class CommandCenter : ICommandCenter
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public CommandCenter(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        /**
+         * SendCommand provides a global API
+         */
+        public void SendCommand(string command)
+        {
+            var commandExecuters = GetCommandExecutersInAssembly();
+
+            var executer = commandExecuters.SingleOrDefault(x => x.MatchCommand(command));
+            executer?.ExecuteCommand(command);
+        }
+
+        /**
+         * GetCommandExecutersInAssembly gathers all classes that base of CommandExecuter, and instantiates them
+         * with _serviceProvider
+         * _serviceProvider contains singletons of Plateau and RoverSquadManager
+         */
+        private IEnumerable<CommandExecuter> GetCommandExecutersInAssembly()
+        {
+            return Assembly.GetExecutingAssembly()
+                .DefinedTypes
+                .Where(type => type.IsSubclassOf(typeof(CommandExecuter)) && !type.IsAbstract)
+                .Select(x => Activator.CreateInstance(x, _serviceProvider) as CommandExecuter)
+                .ToList();
+            
+            // Classes that implement abstract class CommandExecuter
+            // PlataeuCommandExecuter 
+            // RoverCommandExecuter 
+            // RoverSquadCommandExecuter 
+            
+        }
+    }
+}
---
+++ MarsRover/Plateau.cs
@@ -0,0 +1,12 @@
+﻿namespace MarsRover
+{
+    public class Plateau : ILandingSurface
+    {
+        public SurfaceSize Size { get; private set; }
+        
+        public void Define(int width, int height)
+        {
+            Size = new SurfaceSize(width, height);
+        }
+    }
+}
